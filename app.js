a340230fe1cbacd8e1b16e4ca8edcd8d1865f21b
RichardCh971 <142849499+RichardCh971@users.noreply.github.com>
2024-06-05 16:54:57 +0000

    added a navbar and a js file

---
+++ app.js
@@ -0,0 +1,9 @@
+const menu = document.querySelector('#mobile-menu')
+const menuLinks = document.querySelector('.navbar__menu')
+const menuLinks2 = document.querySelector('.navbar')
+
+menu.addEventListener('click', function() {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+    menuLinks2.classList.toggle('active');
+});
