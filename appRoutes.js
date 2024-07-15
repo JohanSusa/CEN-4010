fd26729f497d9b59700d8a531773295d3c611f63
RichardCh971 <142849499+RichardCh971@users.noreply.github.com>
2024-07-15 17:10:35 +0000

    added different js files for tracker and google api because it was causing bugs so i fixed them.

---
+++ appRoutes.js
@@ -0,0 +1,35 @@
+const menu = document.querySelector('#mobile-menu')
+const menuLinks = document.querySelector('.navbar_menu')
+const menuLinks2 = document.querySelector('.navbar')
+
+const nav = document.querySelector('.navbar')
+let lastScrollY = window.scrollY;
+
+menu.addEventListener('click', function() {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+    menuLinks2.classList.toggle('active');
+});
+
+window.addEventListener("scroll", () => {
+    if(lastScrollY < window.scrollY){
+        nav.classList.add("navHidden");
+    } else {
+        nav.classList.remove("navHidden")
+    }
+    lastScrollY = window.scrollY;
+});
+
+
+//GOOGLE API        https://developers.google.com/maps/documentation/javascript/overview#javascript
+let map;
+
+async function initMap() {
+  const { Map } = await google.maps.importLibrary("maps");
+
+  map = new Map(document.getElementById("map"), {
+    center: { lat: 25.75798389937088, lng: -80.37469065220539 },
+    zoom: 18,
+  });
+}
+initMap();
